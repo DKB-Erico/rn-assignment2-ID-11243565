ec59486b2608cdb7ada8440d79b385f1ee7356b4
Biadela Deklu <bedeklu@st.ug.edu.gh>
2024-05-26 15:50:58 +0000

    Third commit: Text size change

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is Deklu Biadela Eric</Text>
+      <Text style={styles.text}>My name is Deklu Biadela Eric</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+    text: {
+      fontSize: 24,
+    },
 });
