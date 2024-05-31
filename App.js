da1c7e0f436bb7a64025ed529f5def4211b90557
Biadela Deklu <bedeklu@st.ug.edu.gh>
2024-05-31 23:19:54 +0000

    Final commit: Name appearance change

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is <Text style={styles.boldText}>Deklu Biadela</Text></Text>
+      <Text style={styles.text}>My name is <Text style={styles.boldText}>Deklu Biadela Eric</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
